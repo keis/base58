a6a03885dfa1e1375d310c1370c918dfbdd17ff4
David Keijser <keijser@gmail.com>
2016-06-14 10:35:24 +0200

    Enhance error message with type name

--- base58.py
+++ base58.py
@@ -31,7 +31,8 @@ def b58encode(v):
     '''Encode a string using Base58'''
 
     if not isinstance(v, bytes):
-        raise TypeError("a bytes-like object is required, not 'str'")
+        raise TypeError("a bytes-like object is required, not '%s'" %
+                        type(v).__name__)
 
     origlen = len(v)
     v = v.lstrip(b'\0')
