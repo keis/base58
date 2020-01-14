cb7a30e1f5136f30021de777d87774b43ba77aae
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-01-14 12:16:11 +0100

    Distribute type data (PEP 561)

--- base58/__init__.py
+++ base58/__init__.py
@@ -1,6 +1,6 @@
 '''Base58 encoding
 
-Implementations of Base58 and Base58Check endcodings that are compatible
+Implementations of Base58 and Base58Check encodings that are compatible
 with the bitcoin network.
 '''
 
@@ -128,49 +128,3 @@ def b58decode_check(
         raise ValueError("Invalid checksum")
 
     return result
-
-
-def main():
-    '''Base58 encode or decode FILE, or standard input, to standard output.'''
-
-    import sys
-    import argparse
-
-    stdout = sys.stdout.buffer
-
-    parser = argparse.ArgumentParser(description=main.__doc__)
-    parser.add_argument(
-        'file',
-        metavar='FILE',
-        nargs='?',
-        type=argparse.FileType('r'),
-        default='-')
-    parser.add_argument(
-        '-d', '--decode',
-        action='store_true',
-        help='decode data')
-    parser.add_argument(
-        '-c', '--check',
-        action='store_true',
-        help='append a checksum before encoding')
-
-    args = parser.parse_args()
-    fun = {
-        (False, False): b58encode,
-        (False, True): b58encode_check,
-        (True, False): b58decode,
-        (True, True): b58decode_check
-    }[(args.decode, args.check)]
-
-    data = args.file.buffer.read()
-
-    try:
-        result = fun(data)
-    except Exception as e:
-        sys.exit(e)
-
-    stdout.write(result)
-
-
-if __name__ == '__main__':
-    main()
