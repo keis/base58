7f06a86287b5b88ffdb07e9dd8a7a4d92c238721
tanupoo <github-shoichi@tanu.org>
2021-01-07 00:24:16 +0900

    made it handle any base with passing alphabet.

--- base58/__init__.py
+++ base58/__init__.py
@@ -41,8 +41,9 @@ def b58encode_int(
     if not i and default_one:
         return alphabet[0:1]
     string = b""
+    base = len(alphabet)
     while i:
-        i, idx = divmod(i, 58)
+        i, idx = divmod(i, base)
         string = alphabet[idx:idx+1] + string
     return string
 
@@ -94,10 +95,10 @@ def b58decode_int(
     map = _get_base58_decode_map(alphabet, autofix=autofix)
 
     decimal = 0
-
+    base = len(alphabet)
     try:
         for char in v:
-            decimal = decimal * 58 + map[char]
+            decimal = decimal * base + map[char]
     except KeyError as e:
         raise ValueError(
             "Invalid character <{char}>".format(char=chr(e.args[0]))
